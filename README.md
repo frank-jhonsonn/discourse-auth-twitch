9d585fdced558eabf8403939162c4584bae386f0
Frank Jhonson <108815649+frank-jhonsonn@users.noreply.github.com>
2022-08-24 09:31:55 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# discourse-auth-twitch
-Twitch API OAuth for Discourse
+discourse-auth-twitch
+=====================
+
+Twitch Auth for Discourse
+
+Installation Instructions (for Docker installations):
+
+* Open your container app.yml
+* Register a new Twitch API application at https://dev.twitch.tv/console/apps if you haven't already.
+  * For the Redirect API: http(s)://example.com/auth/twitch/callback
+* Under section ```env:``` your Twitch API credentials must be added:
+```
+  TWITCH_CLIENT_ID: CLIENT_ID
+  TWITCH_CLIENT_SECRET: CLIENT_SECRET
+```
+* Under section ```hooks:``` append the following
+```
+          - git clone https://github.com/night/discourse-auth-twitch.git
+```
+* Rebuild the docker container
+```
+./launcher rebuild my_image
+```
---
+++ plugin.rb
@@ -0,0 +1,72 @@
+# name: Twitch
+# about: Authenticate to Discourse with Twitch
+# version: 1.0.2
+# author: Night (nightdev.com)
+
+gem 'omniauth-twitch', '1.1.0'
+
+class TwitchAuthenticator < ::Auth::Authenticator
+
+  CLIENT_ID = ENV["TWITCH_CLIENT_ID"]
+  CLIENT_SECRET = ENV["TWITCH_CLIENT_SECRET"]
+
+  def name
+    'twitch'
+  end
+
+  def after_authenticate(auth_token)
+    result = Auth::Result.new
+
+    # grab the info we need from omni auth
+    data = auth_token[:info]
+    extra = auth_token[:extra]
+    username = data["nickname"]
+    name = data["name"]
+    email = data["email"]
+    twitch_uid = auth_token["uid"]
+
+    # plugin specific data storage
+    current_info = ::PluginStore.get("twitch", "twitch_uid_#{twitch_uid}")
+
+    result.user =
+      if current_info
+        User.where(id: current_info[:user_id]).first
+      end
+
+    result.username = username
+    result.name = name
+    result.email = email
+    result.extra_data = { twitch_uid: twitch_uid }
+
+    result
+  end
+
+  def after_create_account(user, auth)
+    data = auth[:extra_data]
+    ::PluginStore.set("twitch", "twitch_uid_#{data[:twitch_uid]}", {user_id: user.id })
+  end
+
+  def register_middleware(omniauth)
+    omniauth.provider :twitch,
+     CLIENT_ID,
+     CLIENT_SECRET,
+     scope: 'user:read:email'
+  end
+end
+
+auth_provider :title => 'with Twitch',
+    :message => 'Log in with Twitch (Make sure pop up blockers are not enabled).',
+    :frame_width => 920,
+    :frame_height => 800,
+    :authenticator => TwitchAuthenticator.new,
+    :icon => 'fab-twitch'
+
+register_svg_icon "fab fa-twitch" if respond_to?(:register_svg_icon)
+
+register_css <<CSS
+
+.btn-social.twitch {
+  background: #6441A5;
+}
+
+CSS
